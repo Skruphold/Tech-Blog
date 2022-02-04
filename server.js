6495e8a156857ec8bb9bca06f431af7e12123704
Skruphold <hunterco999@gmail.com>
2022-02-03 22:01:27 -0600

    another attempt to fix the heroku page

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
-sequelize.sync({ force: true }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+    sequelize.sync({ force: true });
 });
